c34269711aea0f414915d6aa707ef7949285a43b
anirudh-mk <its.me.anirudh.mk@gmail.com>
2024-07-26 20:38:05 +0530

    feat(user login api added)

--- api/urls.py
+++ api/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from api import views
 
 urlpatterns = [
-    path('user/register/', views.UserRegisterAPI.as_view())
+    path('user/register/', views.UserRegisterAPI.as_view()),
+    path('user/login/', views.UserLoginAPI.as_view()),
 ]
--- api/views.py
+++ api/views.py
@@ -3,6 +3,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from api.serializer import UserRegisterSerializer
+from django.contrib.auth import authenticate
 
 
 # Create your views here.
@@ -18,3 +19,35 @@ class UserRegisterAPI(APIView):
             )
 
         return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class UserLoginAPI(APIView):
+    def post(self, request):
+        username = request.data.get('username')
+        password = request.data.get('password')
+
+        if username is None:
+            return Response(
+                data={"please enter your username"},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        if password is None:
+            return Response(
+                data={'please enter your password'},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        user = authenticate(username=username, password=password)
+
+        if user:
+            return Response(
+                data={'user login successfully'},
+                status=status.HTTP_200_OK
+            )
+
+        else:
+            return Response(
+                data={'invalid username or password'}, 
+                status=status.HTTP_400_BAD_REQUEST
+            )
