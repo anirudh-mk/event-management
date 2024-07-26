f1257cccd6add1d5869ee81457bd506d7eefd7d9
anirudh-mk <its.me.anirudh.mk@gmail.com>
2024-07-26 19:51:46 +0530

    feat(registration model added)

--- api/models.py
+++ api/models.py
@@ -3,6 +3,7 @@ import uuid
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+
 # Create your models here.
 
 
@@ -12,6 +13,10 @@ class User(AbstractUser):
     class Meta:
         db_table = 'user'
 
+    @property
+    def full_name(self):
+        return self.first_name + "" + self.last_name
+
 
 class Event(models.Model):
     id = models.CharField(primary_key=True, max_length=36, default=uuid.uuid4())
@@ -20,6 +25,19 @@ class Event(models.Model):
     date = models.DateTimeField()
     created_at = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return self.title
+
     class Meta:
         db_table = 'event'
         ordering = ['created_at']
+
+
+class Registration(models.Model):
+    id = models.CharField(primary_key=True, max_length=36, default=uuid.uuid4())
+    event = models.ForeignKey(Event, related_name='registration_event', on_delete=models.CASCADE)
+    user = models.ForeignKey(User, related_name='registraiton_user', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = 'registration'
