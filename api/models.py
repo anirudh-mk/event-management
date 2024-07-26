6f352092b83ad6655372c68f08eea30319c97fa3
anirudh-mk <its.me.anirudh.mk@gmail.com>
2024-07-26 19:38:09 +0530

    feat(user model added)

--- api/models.py
+++ api/models.py
@@ -1,3 +1,13 @@
+import uuid
+
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+
+class User(AbstractUser):
+    id = models.CharField(primary_key=True, max_length=36, default=uuid.uuid4())
+
+    class Meta:
+        db_table = 'user'
